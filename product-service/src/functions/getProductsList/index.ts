eaf83fa7631cd04ed59c6532d4b2a751bfe36a9a
epalekz <alejandro_aragon@epam.com>
2023-03-05 23:13:25 -0600

    task 3

---
+++ product-service/src/functions/getProductsList/index.ts
@@ -0,0 +1,30 @@
+import { handlerPath } from "@libs/handler-resolver";
+
+// TODO: check if schema is correct?
+export default {
+  handler: `${handlerPath(__dirname)}/handler.main`,
+  events: [
+    {
+      httpApi: {
+        method: "get",
+        path: "/products",
+        documentation: {
+          summary: "Returns list of products",
+          description: "Returns an array containing all existing products.",
+          tags: ["products"],
+          methodResponses: [
+            {
+              statusCode: 200,
+              responseBody: {
+                description: "An array of Product objects",
+              },
+              responseModels: {
+                "application/json": "getProductsListResponse",
+              },
+            },
+          ],
+        },
+      },
+    },
+  ],
+};
